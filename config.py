4258a4264537e4c69f1aaed42b9514a38c9b11bb
jalaine329 <100935722+jalaine329@users.noreply.github.com>
2024-02-01 14:13:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # elastic cluster info
+# change this file to reflect each of your clusters and the urls that are associated with the elasticsearch nodes you would want to send these requests/commands to
 clusters = {
     "cluster1" : ["https://node01.com:9200"],
     "cluster2": ["https://node01.com:9200", "https://node02.com:9200", "https://node03.com:9200"],
