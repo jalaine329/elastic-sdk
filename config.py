1bc62eb65c2e40f5a61b90b4aaf8d656b4575916
jalaine329 <100935722+jalaine329@users.noreply.github.com>
2024-02-01 14:02:02 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# elastic cluster info
+clusters = {
+    "cluster1" : ["https://node01.com:9200"],
+    "cluster2": ["https://node01.com:9200", "https://node02.com:9200", "https://node03.com:9200"],
+    "cluster3": ["https://node01.com:9200", "https://node02.com:9200", "https://node03.com:9200"]
+}
